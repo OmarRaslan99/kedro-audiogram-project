533423333a266d233551ee951801f739c8bf8037
rsciandra4 <romainsciandra@gmail.com>
2026-02-05 15:31:47 +0100

    feat: pipeline evaluation fonctionnel avec calcul du F1-score

--- audiogram-mlops/src/audiogram_mlops/pipelines/evaluation/pipeline.py
+++ audiogram-mlops/src/audiogram_mlops/pipelines/evaluation/pipeline.py
@@ -7,16 +7,10 @@ from kedro.pipeline import Pipeline, node, pipeline
 from .nodes import evaluate_model
 
 def create_pipeline(**kwargs) -> Pipeline:
-    """
-    Crée le pipeline d'évaluation pour valider la robustesse du modèle.
-   
-    """
     return pipeline([
         node(
             func=evaluate_model,
-            # Ces entrées doivent exister dans ton catalog.yml
-            inputs=["ml_model", "test_data", "test_labels"],
-            # La sortie sera sauvegardée dans le dataset défini dans le catalogue
+            inputs=["ml_model", "X_test", "y_test"], 
             outputs="performance_metrics",
             name="evaluate_model_node",
         ),
